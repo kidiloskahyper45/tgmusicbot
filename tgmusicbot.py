e1914e9b6478eb3330094a45d3b7f299f253c028
KiDiLoSkA <73132025+kidiloskahyper45@users.noreply.github.com>
2021-05-13 20:13:45 +0530

    Update tgmusicbot.py

--- tgmusicbot.py
+++ tgmusicbot.py
@@ -29,10 +29,10 @@ youtube-dl
 
 # ../../config.py
 MUSIC_CHATS = [
-    -1234567891012,
+    -1001165653988,
     -2345678910123
 ]
-MUSIC_USERS = [1234567890]
+MUSIC_USERS = [1501753145]
 MUSIC_DELAY_DELETE_INFORM = 10
 MUSIC_INFORM_AVAILABILITY = (
     "This bot only serves the specified group and"
